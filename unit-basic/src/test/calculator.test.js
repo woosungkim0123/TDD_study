13899856673b93daa9abb62811885fd5cce7a1dc
woosungkim0123 <sibu2005@naver.com>
2022-08-02 22:23:49 +0900

    calculate test example use jest

---
+++ unit-basic/src/test/calculator.test.js
@@ -0,0 +1,57 @@
+const Calculator = require("../calculator.js");
+
+describe("Calculator", () => {
+  let cal;
+
+  beforeEach(() => {
+    cal = new Calculator();
+  });
+
+  it("inits with 0", () => {
+    expect(cal.value).toBe(0);
+  });
+
+  it("sets", () => {
+    cal.set(9);
+    expect(cal.value).toBe(9);
+  });
+  it("clear", () => {
+    cal.set(9);
+    cal.clear();
+    expect(cal.value).toBe(0);
+  });
+  it("adds", () => {
+    cal.set(9);
+    cal.add(1);
+    expect(cal.value).toBe(10);
+  });
+
+  it("subtract", () => {
+    cal.set(9);
+    cal.subtract(1);
+    expect(cal.value).toBe(8);
+  });
+
+  it("multiply", () => {
+    cal.set(9);
+    cal.multiply(1);
+    expect(cal.value).toBe(9);
+  });
+
+  describe("divides", () => {
+    it("0 / 0 === NaN", () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    });
+    it("1 / 0 === Infinity", () => {
+      cal.set(1);
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    });
+    it("4 / 4 === 1", () => {
+      cal.set(4);
+      cal.divide(4);
+      expect(cal.value).toBe(1);
+    });
+  });
+});
