4dae2c1e44a44646c89d5dc0aa959e0ac3e70cdf
woosungkim0123 <sibu2005@naver.com>
2022-08-07 23:22:41 +0900

    refactoring

--- unit-basic/src/challenge/stack.js
+++ unit-basic/src/challenge/stack.js
@@ -1,24 +1,30 @@
 class Stack {
   constructor() {
-    this.array = [];
+    this._size = 0;
+    this.head = null; // 다음 item을 가르킬 수 있는 변수
   }
   size() {
-    return this.array.length;
+    return this._size;
   }
   push(word) {
-    this.array.push(word);
+    const node = { word, next: this.head };
+    this.head = node;
+    this._size++;
   }
   pop() {
-    if (this.array.length === 0) {
+    if (this.head === null) {
       throw new Error("Stack is empty");
     }
-    return this.array.pop();
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.word;
   }
   peek() {
-    if (this.array.length === 0) {
+    if (this.head === null) {
       throw new Error("Stack is empty");
     }
-    return this.array[this.size() - 1];
+    return this.head.word;
   }
 }
 
